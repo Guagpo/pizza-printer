3c077ba69f81f8b66212af8f5987238508bd8ec0
subhanlayros123-ux <subhanlayros123@gmail.com>
2026-03-22 13:50:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const fetch = require('node-fetch');
-
-const app = express();
-app.use(express.json());
+const axios = require('axios');
 
 app.post('/print-order', async (req, res) => {
   try {
     console.log('RAW:', JSON.stringify(req.body));
 
-    // SEND VIDERE TIL DIN PHP
-    await fetch("http://93.93.205.145:8080/order.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(req.body)
-    });
+    await axios.post('http://93.93.205.145:8080/order.php', req.body);
 
     res.json({ success: true });
   } catch (err) {
     console.error('Fejl:', err.message);
-    res.status(500).json({ success: false });
+    res.status(500).json({ success: false, error: err.message });
   }
 });
-
-app.listen(3000);
