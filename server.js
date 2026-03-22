aa60c7dba8337f04e89a68e0ed1455ba637f0821
subhanlayros123-ux <subhanlayros123@gmail.com>
2026-03-22 13:03:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const fetch = require('node-fetch');
+
 const app = express();
 app.use(express.json());
 
 app.post('/print-order', async (req, res) => {
   try {
     console.log('RAW:', JSON.stringify(req.body));
+
+    // SEND VIDERE TIL DIN PHP
+    await fetch("http://93.93.205.145:8080/order.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
     res.json({ success: true });
   } catch (err) {
     console.error('Fejl:', err.message);
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false });
   }
 });
 
-app.listen(3000, () => console.log('Server kører på http://localhost:3000'));
+app.listen(3000);
